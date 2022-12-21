489cc7da9a485ab94cb9b1f1f573d854757fde15
Sheev <ericmarcypro@gmail.com>
2022-12-20 19:53:42 -0500

    added package.json, established server.js connection

--- server.js
+++ server.js
@@ -9,3 +9,15 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// Connection to db established
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'P1g$oUnDSQL!',
+        database: 'employees_db'
+    },
+    // logs connection to confirm connectivity 
+    console.log(`Connected to employees_db`)
+);
